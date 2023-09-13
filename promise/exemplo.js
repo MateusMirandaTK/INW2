2f6fa0f9ea0cddf3c14a43317eddd917e0221665
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-13 10:51:37 -0300

    promise

---
+++ promise/exemplo.js
@@ -0,0 +1,11 @@
+function falarDepoisDE(segundos, frase){
+return new Promise((resolve, reject)=>{
+  setTimeout(()=>{
+            resolve(frase)
+        },segundos * 1000);
+    });
+}
+
+falarDepoisDE(4, "Teste da promisse no 2mib")
+.then(frase => frase.concat(' programando em vscode!!!'))
+.then(outraFrase => console.log(outraFrase))
